d4ba9684accc5fdeb1638fe22fe314b049694d61
Yacquub egal <97262950+Yacquubegal@users.noreply.github.com>
2022-05-01 16:43:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,16 @@
 // code the runs immediately when page is first loaded
 
 // define a Constructor function
+    // this.someName = parseInt(parameterName);
+
+
+let CustomerArray = []; // define an array to hold Customer objects
+
+
 var Fooditem = function(pfoodname, pCalories) {
     this.foodname = pfoodname;
     this.Calories = pCalories;
-    // this.someName = parseInt(parameterName);
-};
-
-CustomerArray = []; // define an array to hold Customer objects
-
- 
-
-// pr-load array with 2 objects
-let newFooditem0 = new Fooditem("Banana", 20)
-CustomerArray.push(newFooditem0);
-
-let newFooditem1 = new Fooditem("Hot dog", 458);
-CustomerArray.push(newFooditem1);
-
-let newFooditem2 = new Fooditem("cake", 200);
-CustomerArray.push(newFooditem2);
-
-
-document.addEventListener("DOMContentLoaded", function (event) {
-
-// define what should happend when the "show one" button is clicked
-    // document.getElementById("show").addEventListener("click", function () {
-    //     console.log(CustomerArray);
-    //     let message = "foodname  Calories\n";  // add heading
-    //     // search thru array to find which object has the correct name property
-    //     for (i = 0; i < CustomerArray.length; i++) {
-    //         if( document.getElementById("whichPerson").value == CustomerArray[i].foodname){
-    //         message = message + CustomerArray[i].foodname + "  " + CustomerArray[i].Calories;
-    //         console.log(message);
-    //         }
-    //     }
-    //     document.getElementById("output").value = message;  // write out the data
-    // });
-    
-});
+}
 
 // define what should happen when the form is submitted
 function formSubmitEvent() {
@@ -49,9 +21,18 @@ function formSubmitEvent() {
     // clear text boxes
     document.getElementById("foodname").value = "";
     document.getElementById("Calories").value = "";
+    let message = "foodname  Calories\n";  // add heading
+    // search thru array to find which object has the correct name property
+    
+    let totalCalories = 0;
+    for (i = 0; i < CustomerArray.length; i++) {
+        console.log(CustomerArray[i].Calories);
+        totalCalories += parseInt(CustomerArray[i].Calories);
+    }
+    document.getElementById("TotalofCalories").value = totalCalories;  // write out the data
+  
     // at this point you have just added another item to the array
     // loop thru the array and add up the calorie count for each object in the array
     // and then write that out to the (very badly named)
     // <input name="whichPerson" id="whichPerson" /><br>
         }
-  
